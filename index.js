07aedc6b14a18f1ddf518c1dc17af29136eda721
Kamal <kamal.raghe33@gmail.com>
2024-09-17 12:15:25 -0400

    commit

--- index.js
+++ index.js
@@ -9,34 +9,38 @@
     
 
     function reverse2(){
-        let anagram
-       
-        const buildCharMap = (str)=>{
-            const charMap = new Map()           
-            for(const char of clean(str)){
-             charMap.set(char, charMap.get(char) + 1 || 1)
-            }
-            return charMap 
-         }
-         const clean = (str) => {
-             return str.toLowerCase().replaceAll(' ','')
-         }
-
-        let CharMapA = buildCharMap(input.value)
-        let CharMapB = buildCharMap(input2.value)
-        
-        anagram = true
-
-        if(CharMapA.size !== CharMapB.size){
-             anagram = false
-        }
-
-        for(const [char,count] of CharMapA){
-            if(CharMapB.get(char) !== count){
-                anagram = false
-            }
             
-        } 
+        const clean = (str) => {
+            return str.toLowerCase().replaceAll(' ','').split('').sort().join('')
+        }
+        let anagram = (clean(input.value) === clean(input2.value)) 
+
+        // const buildCharMap = (str)=>{
+        //     const charMap = new Map()           
+        //     for(const char of clean(str)){
+        //      charMap.set(char, charMap.get(char) + 1 || 1)
+        //     }
+        //     return charMap 
+        //  }
+        //  const clean = (str) => {
+        //      return str.toLowerCase().replaceAll(' ','')
+        //  }
+
+        // let CharMapA = buildCharMap(input.value)
+        // let CharMapB = buildCharMap(input2.value)
+        
+        // anagram = true
+
+        // if(CharMapA.size !== CharMapB.size){
+        //      anagram = false
+        // }
+
+        // for(const [char,count] of CharMapA){
+        //     if(CharMapB.get(char) !== count){
+        //         anagram = false
+        //     }
+            
+        // } 
  
         if(anagram){
             anagram = 'yes'
